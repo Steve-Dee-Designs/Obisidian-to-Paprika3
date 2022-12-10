ffd0682a00ed230b44634a04238f55a662356033
Steve Dogiakos <dogiakos@gmail.com>
2022-12-09 17:08:32 -0700

    Added prompt for the folder path

--- convert_md_to_paprika3.py
+++ convert_md_to_paprika3.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+
 def convert_md_to_paprika3(file_path):
     # read the contents of the file
     with open(file_path, "r") as f:
@@ -8,15 +9,20 @@ def convert_md_to_paprika3(file_path):
     # perform formatting operations on the text
     formatted_contents = re.sub(r"# (.*)", r"<h1>\1</h1>", contents)
     formatted_contents = re.sub(r"## (.*)", r"<h2>\1</h2>", formatted_contents)
-    formatted_contents = re.sub(r"### (.*)", r"<h3>\1</h3>", formatted_contents)
-    formatted_contents = re.sub(r"\*\*(.*)\*\*", r"<strong>\1</strong>", formatted_contents)
+    formatted_contents = re.sub(
+        r"### (.*)", r"<h3>\1</h3>", formatted_contents)
+    formatted_contents = re.sub(
+        r"\*\*(.*)\*\*", r"<strong>\1</strong>", formatted_contents)
     formatted_contents = re.sub(r"_(.*)_", r"<em>\1</em>", formatted_contents)
     # write the formatted text to a new file
     with open(file_path[:-3] + ".paprika3", "w") as f:
         f.write(formatted_contents)
 
+
+# prompt the user for the folder path
+folder_path = input("Enter the path to the folder: ")
+
 # get a list of all the files in the folder
-folder_path = "/path/to/folder"
 files = os.listdir(folder_path)
 # convert each file to Paprika3 format
 for file in files:
